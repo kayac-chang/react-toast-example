5e71a62301d2708cc238b19a805fdc4d25dee4cb
kayac-chang <egg734631@gmail.com>
2021-09-11 22:05:56 +0800

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,30 @@
-import React, {
-  ReactNode,
-  createContext,
-  useState,
-  useContext,
-  useCallback,
-} from "react";
-import { Background, Toast } from "./components";
-import { v4 as uuid } from "uuid";
-
-type Emit = (message: string) => void;
-const Context = createContext<Emit | undefined>(undefined);
-
-type ToastProviderProps = {
-  children?: ReactNode;
-};
-function ToastProvider({ children }: ToastProviderProps) {
-  const [messages, setMessages] = useState<{ id: string; message: string }[]>(
-    []
-  );
-
-  const setMessage = useCallback(
-    (message) => {
-      if (!message) return;
-
-      const id = uuid();
-
-      setMessages((queue) => [...queue, { id, message }]);
-
-      setTimeout(() => {
-        setMessages((queue) => queue.filter((pair) => pair.id !== id));
-      }, 3000);
-    },
-    [setMessages]
-  );
-
-  return (
-    <Context.Provider value={setMessage}>
-      {children}
-
-      {messages.map((message, index) => (
-        <Toast type="success" key={index} />
-      ))}
-    </Context.Provider>
-  );
-}
-
-function useToast() {
-  const context = useContext(Context);
-
-  if (!context) throw new Error("useToast should be use within ToastProvider");
-
-  return context;
-}
+import React from "react";
+import { Background, Icon, ToastProvider, useToast } from "./components";
 
 function Main() {
   const setMessage = useToast();
 
   return (
     <main
-      className="relative z-10 w-screen h-screen"
-      onClick={() => setMessage("click")}
-    ></main>
+      className="relative z-10 w-screen h-screen container mx-auto"
+      onClick={() => setMessage({ type: "success", message: "hello" })}
+    >
+      <header className="absolute bottom-0 m-[4rem] flex flex-col gap-2">
+        <div className="relative w-24 text-yellow -mx-4">
+          <span>
+            <Icon.Toast />
+          </span>
+
+          <span className="absolute top-0 w-full transform -translate-y-1 translate-x-[54%] rotate-[30deg]">
+            <Icon.Toast />
+          </span>
+        </div>
+
+        <h1 className="font-black text-5xl text-yellow">TOAST</h1>
+
+        <p className="text-white text-lg">A delicious addition to your UI.</p>
+      </header>
+    </main>
   );
 }
 
--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,17 +1,41 @@
-import React from "react";
+import React, {
+  ReactNode,
+  createContext,
+  useState,
+  useContext,
+  useCallback,
+  useEffect,
+} from "react";
 import { Icon } from ".";
 import clsx from "clsx";
 import { createPortal } from "react-dom";
+import { v4 as uuid } from "uuid";
+import { pipe, not, propEq, filter, append } from "ramda";
 
-type ToastProps = {
-  type: "success" | "warning" | "danger";
-};
-export function Toast({ type }: ToastProps) {
+const excludeByID = (id: string) => filter(pipe(propEq("id", id), not));
+
+const wait = (duration: number) =>
+  new Promise((resolve) => setTimeout(resolve, duration));
+
+const TAG = "toast";
+
+type MessageType = "success" | "warning" | "danger";
+interface Message {
+  id: string;
+  type: MessageType;
+  message: string;
+  duration?: number;
+}
+type Emit = (message: Omit<Message, "id">) => void;
+
+const Context = createContext<Emit | undefined>(undefined);
+
+type ToastProps = Omit<Message, "id" | "duration">;
+function Toast({ type, message }: ToastProps) {
   return createPortal(
     <div
       className={clsx(
-        "relative",
-        "flex items-center gap-4 p-2 rounded w-80 border-2",
+        "flex items-center gap-4 p-2 rounded w-80 border-2 relative",
         type === "success" && "bg-white border-green text-green",
         type === "warning" && "bg-yellow border-yellow text-yellow-darkest",
         type === "danger" && "bg-red border-red text-white"
@@ -25,13 +49,73 @@ export function Toast({ type }: ToastProps) {
 
       <div>
         <strong>Toast is ready</strong>
-        <p className="opacity-50">Hmm, it fully baked.</p>
+        <p className="opacity-50">{message}</p>
       </div>
 
       <span className="absolute right-0 top-0 w-4 m-2">
         <Icon.X />
       </span>
     </div>,
-    document.getElementById("alert") as HTMLElement
+    document.getElementById(TAG) as HTMLElement
   );
+}
+
+function initial() {
+  if (document.getElementById(TAG)) return;
+
+  const wrapper = document.createElement("div");
+  wrapper.id = TAG;
+  wrapper.classList.add(
+    ...clsx(
+      "fixed top-0 z-50 pointer-events-none",
+      "w-screen h-screen flex flex-col items-end",
+      "p-8 gap-2"
+    ).split(" ")
+  );
+
+  document.body.append(wrapper);
+}
+
+type ToastProviderProps = {
+  children?: ReactNode;
+  defaultDuration?: number;
+};
+export function ToastProvider({
+  children,
+  defaultDuration = 10000,
+}: ToastProviderProps) {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const emit: Emit = useCallback(
+    (message) => {
+      const id = uuid();
+
+      setMessages(append({ id, ...message }));
+
+      wait(message.duration || defaultDuration)
+        //
+        .then(() => setMessages(excludeByID(id)));
+    },
+    [setMessages]
+  );
+
+  useEffect(initial, []);
+
+  return (
+    <Context.Provider value={emit}>
+      {children}
+
+      {messages.reverse().map(({ id, ...props }) => (
+        <Toast {...props} />
+      ))}
+    </Context.Provider>
+  );
+}
+
+export function useToast() {
+  const context = useContext(Context);
+
+  if (!context) throw new Error("useToast should be use within ToastProvider");
+
+  return context;
 }
